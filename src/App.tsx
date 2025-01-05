6dd649847670455e90dce6cd7f0e273c0626df1b
Ale <alejandroabdala2000@gmail.com>
2025-01-05 18:59:39 -0300

    buen mvp

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import MainContainer from "./components/mainContainer/MainContainer";
+
+
+import './App.css'
+
+function App() {
+
+  return (
+      <MainContainer />
+  )
+}
+
+export default App;
--- src/components/mainContainer/heroText/HeroText.tsx
+++ src/components/mainContainer/heroText/HeroText.tsx
@@ -9,13 +9,16 @@ function HeroText() {
                     <h1>¡Bienvenido a mi Portfolio!</h1>
                 </div>
 
+                <div className='heroText'>
+                    <p>Soy Ale, un desarrollador de software apasionado por la tecnología.</p>
+                </div>
+
                 <div className='heroImg'>
                     <img src="src/assets/bgsvg.svg" alt="" />
                 </div>
 
 
                 <div className='heroText'>
-                    <p>Soy Ale, un desarrollador de software apasionado por la tecnologia.</p>
                     <p>Vivo en Tandil, provincia de Buenos Aires, Argentina.</p>
                     <p>Actualmente estoy terminando mi carrera como desarrollador de aplicaciones en la universidad de ciencias exactas de Unicen en Tandil.</p>
                     <p>Tambien, trabajo de forma remota como desarrollador backend en Apexlict, una empresa de desarrollo de software ubicada en Guayaquil, Ecuador.</p>
