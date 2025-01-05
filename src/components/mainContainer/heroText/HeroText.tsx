a43c612f3f108f9624720609562fcec9bddfabb3
Ale <alejandroabdala2000@gmail.com>
2025-01-05 20:44:31 -0300

    c

--- src/components/mainContainer/heroText/HeroText.tsx
+++ src/components/mainContainer/heroText/HeroText.tsx
@@ -20,7 +20,7 @@ function HeroText() {
 
                 <div className='heroText'>
                     <p>Vivo en Tandil, provincia de Buenos Aires, Argentina.</p>
-                    <p>Actualmente estoy terminando mi carrera como desarrollador de aplicaciones en la universidad de ciencias exactas de Unicen en Tandil.</p>
+                    <p>Actualmente estoy terminando mi carrera como desarrollador de aplicaciones en la universidad de ciencias exactas de UNICEN en Tandil.</p>
                     <p>También, trabajo de forma remota como desarrollador backend en Apexlict, una empresa de desarrollo de software ubicada en Guayaquil, Ecuador.</p>
                 </div>
             </div>
