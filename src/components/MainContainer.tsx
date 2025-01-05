6ded6cd3f54fc43887a441437d1e456c2f8335fc
Ale <alejandroabdala2000@gmail.com>
2025-01-04 22:04:38 -0300

    cambios

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -9,6 +9,11 @@ function MainContainer() {
             <div className='heroText'>
                 <h1>¡Bienvenido a mi Portfolio!</h1>
             </div>
+            <div className='heroSubtext'>
+                <p>Soy Ale, un desarrollador de software apasionado por lo que hace.</p> 
+                <br/>
+                <p>Vivo en Tandil, provincia de Buenos Aires, Argentina.</p>
+            </div>
             
         </div>
         </>
