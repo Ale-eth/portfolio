ea332eecfa5bb4361b1d54a7924dbb64a028834b
Ale <alejandroabdala2000@gmail.com>
2025-01-04 21:42:57 -0300

    cambios

---
+++ src/components/MainContainer.tsx
@@ -0,0 +1,18 @@
+import './mainContainer.css'
+import Navbar from './Navbar';
+
+function MainContainer() {
+    return (
+        <>
+        <Navbar />
+        <div className="mainContainer">
+            <div className='heroText'>
+                <h1>¡Bienvenido a mi Portfolio!</h1>
+            </div>
+            
+        </div>
+        </>
+    )
+  }
+
+export default MainContainer;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import './navbar.css'
+
+
+function Navbar() {
+    return (
+        <>
+        <div className="navBar">
+            <ul>
+                <li>HOME</li>
+                <li>ABOUT ME</li>
+                <li>PROJECTS</li>
+                <li>CONTACT</li>
+            </ul>
+        </div>
+        </>
+    )
+  }
+
+export default Navbar;
